2abb0c7485dcfe70e17c7b320a5c68107fcc48bd
Yana Mazur <mazurihaa@yandex.ru>
2026-02-24 20:41:56 +0300

    Built the project architecture

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from config import DEFAULT_TIMEOUT
+
+@pytest.fixture
+def browser():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def wait(browser):
+    return WebDriverWait(browser, DEFAULT_TIMEOUT)  #явное ожидание
