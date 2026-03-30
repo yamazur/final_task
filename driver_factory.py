8bed8436b65c76ce5ebd9f4a16517ae8c13c65d4
Yana Mazur <mazurihaa@yandex.ru>
2026-03-30 18:22:40 +0300

    Написала readme, создала файл driver_factory
    # Conflicts:
    #	README.md

---
+++ driver_factory.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+class DriverFactory:
+    @staticmethod
+    def create_driver(browser_name: str):
+        if browser_name == "chrome":
+            return webdriver.Chrome()
+
+        elif browser_name == "firefox":
+            return webdriver.Firefox()
+
+        # elif browser_name == "edge":
+        #     return webdriver.Edge()
+        #
+        # elif browser_name == "ie":
+        #     from selenium.webdriver.ie.options import Options
+        #     options = Options()
+        #     options.ignore_zoom_level = True
+        #     options.ignore_protected_mode_settings = True
+        #     return webdriver.Ie(options=options)
+
+        else:
+            raise ValueError(f"Unsupported browser: {browser_name}")
