46bdeda2fb6dcf503bd0095e8dfc8e3e5dca684b
Yana Mazur <mazurihaa@yandex.ru>
2026-03-31 21:24:11 +0300

    Добавила метод refresh, чтобы не дублировать код и закрывать всплывающие алерты после обновления страницы. Также запустила тесты через FireFox, все успешно

--- run_tests.sh
+++ run_tests.sh
@@ -2,20 +2,22 @@
 
 JAR_PATH="./selenium/selenium-server-4.41.0.jar"
 
-# Запускаем Grid на localhost
+# аргумент — браузер (по умолчанию chrome)
+BROWSER=${1:-chrome}
+
+echo "Браузер: $BROWSER"
+
 echo "Запуск Selenium Grid..."
 java -jar "$JAR_PATH" standalone --selenium-manager true &
 GRID_PID=$!
 
+echo "Ждём запуска Grid..."
 sleep 10
 
-# Устанавливаем URL для тестов
 export GRID_URL="http://localhost:4444"
 
-echo "Запуск тестов через pytest-xdist..."
-pytest tests/ -n 2
+echo "Запуск тестов через Grid..."
+pytest tests/ -n 2 --browser=$BROWSER --remote
 
 echo "Остановка Selenium Grid..."
 kill $GRID_PID
-
-#chmod +x run_tests.sh - запуск в терминале
